0f43d0de6ea55779ab26e6e7caeade775536466d
MrAbade <vini.abade@outlook.com.br>
2024-04-24 06:21:34 -0300

    Bugfix: Fix a bug in gcloud.aio lib factories, provisionally it will create the objects from scratch
    dev

--- domain_payment/frameworks/gcp_storage/manager.py
+++ domain_payment/frameworks/gcp_storage/manager.py
@@ -1,8 +1,12 @@
+import sys
+from pathlib import Path
 from typing import TypedDict
 
 import aiofiles
 import aiohttp
 from gcloud.aio.storage import Storage
+from gcloud.aio.storage.blob import Blob
+from gcloud.aio.storage.bucket import Bucket
 
 from domain_payment.adapters.interface_adapters.interfaces import (
     BucketProvider,
@@ -31,8 +35,21 @@ class GCPStorageManager(BucketProvider, BucketUploader):
     async def upload(self, port: ImageUploadInput) -> ImageUploadOutput:
         async with aiofiles.open(port.path2image, mode="rb") as f:
             output = await f.read()
-            result = await self.__client.upload(port.bucket_name, port.image_name_on_bucket, output)
-            return ImageUploadOutput(image_uri=result["selfLink"])
+            bucket = Bucket(self.__client, port.bucket_name)
+            bucket_metadata = await bucket.get_metadata(session=self.__session)
+            bucket_metadata["size"] = sys.getsizeof(output)
+            blob = Blob(bucket, port.image_name_on_bucket, bucket_metadata)
+            upload_metadata = await blob.upload(output, session=self.__session)
+            signed_image_uri = await Blob(
+                bucket,
+                port.image_name_on_bucket,
+                upload_metadata,
+            ).get_signed_url(
+                1800,
+                session=self.__session,
+            )
+        Path(port.path2image).unlink(missing_ok=True)
+        return ImageUploadOutput(image_uri=signed_image_uri)
 
     def __create_app(self, session: aiohttp.ClientSession) -> Storage:
         if self.__credentials is not None:

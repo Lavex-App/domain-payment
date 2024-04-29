d52ecd9cb573b45a61e4ca3c1de0d045743c3555
MrAbade <vini.abade@outlook.com.br>
2024-04-29 18:15:11 -0300

    Refactor: Improve app contaienrs config readability
    dev

--- domain_payment/containers_config.py
+++ domain_payment/containers_config.py
@@ -7,6 +7,10 @@ from domain_payment.adapters.__factory__ import AdaptersConfig, AdaptersFactory
 from domain_payment.adapters.controllers.__dependencies__ import bind_controller_dependencies
 from domain_payment.business.__factory__ import BusinessFactory
 from domain_payment.frameworks.__factory__ import FrameworksConfig, FrameworksFactory
+from domain_payment.frameworks.firebase import FirebaseFrameworkConfig
+from domain_payment.frameworks.gcp_storage import GCPStorageFrameworkConfig
+from domain_payment.frameworks.mongodb import MotorFrameworkConfig
+from domain_payment.frameworks.pix_efi import PixFrameworkConfig
 
 
 class Config(metaclass=ABCMeta):
@@ -40,15 +44,10 @@ class ProjectConfig(Config):
     @lru_cache
     def frameworks_config(self) -> FrameworksConfig:
         return FrameworksConfig(
-            database_uri=self._env.str("DB_URI"),
-            service_name=self._env.str("SERVICE_NAME"),
-            credentials=self._env.str("GOOGLE_APPLICATION_CREDENTIALS", None),
-            auth_app_options={"projectId": self._env.str("PROJECT_ID")},
-            client_id=self._env.str("CLIENT_ID"),
-            client_secret=self._env.str("CLIENT_SECRET"),
-            certificate=self._env.str("CERTIFICATE"),
-            sandbox=self.is_local or self.is_staging,
-            storage_credentials=self._env.str("GOOGLE_APPLICATION_CREDENTIALS", None),
+            motor_framework_config=self.__motor_framework_config,
+            firebase_framework_config=self.__firebase_framework_config,
+            pix_framework_config=self.__pix_framework_config,
+            gcp_storage_framework_config=self.__gcp_storage_framework_config,
         )
 
     @property
@@ -56,6 +55,48 @@ class ProjectConfig(Config):
     def adapters_config(self) -> AdaptersConfig:
         return AdaptersConfig(
             pix_qrcode_bucket_name=self._env.str("PIX_QRCODE_BUCKET_NAME"),
+        )
+
+    @property
+    @lru_cache
+    def __motor_framework_config(self) -> MotorFrameworkConfig:
+        return (
+            MotorFrameworkConfig(
+                database_uri=self._env.str("DB_URI"),
+                service_name=self._env.str("SERVICE_NAME"),
+                sandbox=self.is_local or self.is_staging,
+            ),
+        )
+
+    @property
+    @lru_cache
+    def __firebase_framework_config(self) -> FirebaseFrameworkConfig:
+        return (
+            FirebaseFrameworkConfig(
+                credentials=self._env.str("GOOGLE_APPLICATION_CREDENTIALS", None),
+                auth_app_options={"projectId": self._env.str("PROJECT_ID")},
+            ),
+        )
+
+    @property
+    @lru_cache
+    def __pix_framework_config(self) -> PixFrameworkConfig:
+        return (
+            PixFrameworkConfig(
+                certificate=self._env.str("CERTIFICATE"),
+                client_id=self._env.str("CLIENT_ID"),
+                client_secret=self._env.str("CLIENT_SECRET"),
+                sandbox=self.is_local or self.is_staging,
+            ),
+        )
+
+    @property
+    @lru_cache
+    def __gcp_storage_framework_config(self) -> GCPStorageFrameworkConfig:
+        return (
+            GCPStorageFrameworkConfig(
+                storage_credentials=self._env.str("GOOGLE_APPLICATION_CREDENTIALS", None),
+            ),
         )
 
 
--- domain_payment/frameworks/__factory__.py
+++ domain_payment/frameworks/__factory__.py
@@ -10,12 +10,18 @@ from .mongodb import MotorFrameworkConfig, MotorManager
 from .pix_efi import PixFrameworkConfig, PixManager
 
 
-class FrameworksConfig(
-    FirebaseFrameworkConfig,
-    MotorFrameworkConfig,
-    GCPStorageFrameworkConfig,
-    PixFrameworkConfig,
-): ...
+class FrameworksConfig:
+    def __init__(
+        self,
+        firebase_framework_config: FirebaseFrameworkConfig,
+        motor_framework_config: MotorFrameworkConfig,
+        gcp_storage_framework_config: GCPStorageFrameworkConfig,
+        pix_framework_config: PixFrameworkConfig,
+    ) -> None:
+        self.firebase_framework_config = firebase_framework_config
+        self.motor_framework_config = motor_framework_config
+        self.gcp_storage_framework_config = gcp_storage_framework_config
+        self.pix_framework_config = pix_framework_config
 
 
 class FrameworksFactory(
@@ -31,7 +37,7 @@ class FrameworksFactory(
 
     def __init__(self, config: FrameworksConfig) -> None:
         self.__config = config
-        self.__manager = MotorManager(config)
+        self.__manager = MotorManager(config.motor_framework_config)
 
     async def connect(self) -> None:
         await self.__manager.connect()
@@ -45,7 +51,7 @@ class FrameworksFactory(
         return self.__manager
 
     def bucket_provider(self) -> GCPStorageManager:
-        return GCPStorageManager(self.__config, self.__session)
+        return GCPStorageManager(self.__config.gcp_storage_framework_config, self.__session)
 
     def authentication_provider(self) -> FirebaseManager:
         return self.__firebase_manager
@@ -54,9 +60,9 @@ class FrameworksFactory(
         return self.__firebase_manager
 
     def pix_provider(self) -> PixManager:
-        return PixManager(self.__config)
+        return PixManager(self.__config.pix_framework_config)
 
     @property
     @lru_cache
     def __firebase_manager(self) -> FirebaseManager:
-        return FirebaseManager(self.__config)
+        return FirebaseManager(self.__config.firebase_framework_config)
